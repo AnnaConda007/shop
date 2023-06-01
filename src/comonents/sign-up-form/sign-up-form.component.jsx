1a787108d819626729c2c7c5a7a3d7de97da20c8
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-01 17:16:06 +0400

    авторизация через пароль , почту

--- src/comonents/sign-up-form/sign-up-form.component.jsx
+++ src/comonents/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import { createAuthWithEmailAndPassword, createUserDocumentFrom } from '../../utils/firebase/firebase.utils';
 const defaultFormField = {
 	displayName: '',
 	email: '',
@@ -9,16 +9,38 @@ const defaultFormField = {
 const SignUpForm = () => {
 	const [formField, setFormField] = useState(defaultFormField);
 	const { displayName, email, password, confirmPassword } = formField;
-
 	console.log(formField);
 	const handleChange = (e) => {
 		const { name, value } = e.target;
 		setFormField({ ...formField, [name]: value }); //расширение объекта при помощи оператора spread
 	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (password !== confirmPassword) {
+			alert('password do not match');
+			return;
+		}
+		try {
+			const { user } = await createAuthWithEmailAndPassword(email, password);
+			await createUserDocumentFrom(user, { displayName });
+			resetFormFields();
+		} catch (error) {
+			if (error.code === 'auth/email-already-in-use') {
+				alert('email already in use');
+			} else {
+				console.log(error);
+			}
+		}
+	};
+
+	const resetFormFields = () => {
+		setFormField(defaultFormField);
+	};
 	return (
 		<div>
 			<h1> Sign up with your email and passwor</h1>
-			<form onSubmit={() => {}}>
+			<form onSubmit={handleSubmit}>
 				<label htmlFor=''>Display name</label>
 				<input required type='text' onChange={handleChange} name='displayName' value={displayName}></input>
 
