af638e699ff2b60ab968ee58b9e95f452914be1e
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-03 15:55:16 +0400

    комментарии

--- src/comonents/sign-in-form/sign-in-form.component.jsx
+++ src/comonents/sign-in-form/sign-in-form.component.jsx
@@ -3,34 +3,37 @@ import { signInWithGooglePopup, createUserDocumentFrom } from '../../utils/fireb
 import FormInput from '../form-input/form-input.component';
 import './sign-in-form.styles.scss';
 import Button from '../button/button.component';
+
 const defaultFormField = {
+	// создается объект для значений полей ввода
 	email: '',
 	password: '',
 };
-
 const SignInForm = () => {
-	const [formField, setFormField] = useState(defaultFormField);
-	const { email, password } = formField;
-	console.log(formField);
+	// ____________________________________блок с обновлением состояний инпутов
+	const [formField, setFormField] = useState(defaultFormField); // создается состояние и изначально оно равно объекту с полями ввода, которые равны ""
+	const { email, password } = formField; // из объекта достаются отдельные значения
 	const handleChange = (e) => {
-		const { name, value } = e.target;
+		const { name, value } = e.target; // из целового события достаются значения  name, value
 		setFormField({ ...formField, [name]: value }); //расширение объекта при помощи оператора spread
+		// при каждом изменении в инпут вызывается handleChange, который сохраняте данные с именем и значением инпута, далее в setFormField каждый раз создается новая весрия массива formField , далее в массиве находится объект с таким же нахванием как name инпута, и этому свойству объкта присваивается значение равное value. Пример - кликаю на инпут с паролем, создается новый formField, у этого инпута name = password, в массиве находится свойство с таким названием и этому свойству записывается значение value. И теперь состяние инпута обновилось, следует ререндеринг
 	};
 
-	const handleSubmit = async (e) => {
+	// ________________________________________блок с отправкой
+	const resetFormFields = () => {
+		setFormField(defaultFormField); // в состояние ипутов записывается изначальный объект, где инпуты = пустой строке
+	};
+	const handleSubmit = async (e) => {//  функция для вызова очищения формы, она вызывается на самой форме
 		e.preventDefault();
-
 		try {
 			resetFormFields();
 		} catch (error) {}
 	};
-	const signInWithGoogle = async () => {
-		const { user } = await signInWithGooglePopup();
-		await createUserDocumentFrom(user);
+	const signInWithGoogle = async () => { // функция авторизации , вызывается, на кнопке 
+		const { user } = await signInWithGooglePopup(); // вызывается метод, определенный в файле firebase, открывается модальное окно гугл с автоматическим настроенными методами для авторизации, так как функции передается метод auth, signInWithGooglePopup возвращает ответ со всеми аданными пользовается, сохраняется свойство user
+		await createUserDocumentFrom(user); // данные из user передаются для дальнейшей обработки в файле в firebase
 	};
-	const resetFormFields = () => {
-		setFormField(defaultFormField);
-	};
+
 	return (
 		<div className='sign-up-container'>
 			<h2>Already have an account</h2>
