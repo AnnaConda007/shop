28f41fd71e7e5ffbc487559714e4a3d0e1a76ef8
Anna Hrulckova <annahrulkova@yandex.ru>
2023-07-06 21:31:54 +0400

    р

--- src/contexts/category.context.jsx
+++ src/contexts/category.context.jsx
@@ -4,17 +4,17 @@ import { getCategoriesAndDocs } from '../utils/firebase/firebase.utils';
 export const CategoryContext = createContext({ categoriesMap: {} });
 
 export const CategoryProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+	const [categoriesMap, setCategoriesMap] = useState({});
 
-  useEffect(() => {
-    const getCategoryMap = async () => {
-      const res = await getCategoriesAndDocs();
-      console.log(res);
-      setCategoriesMap(res);
-    };
-    getCategoryMap();
-  }, []);
+	useEffect(() => {
+		const getCategoryMap = async () => {
+			const res = await getCategoriesAndDocs();
+			console.log(res);
+			setCategoriesMap(res);
+		};
+		getCategoryMap();
+	}, []);
 
-  const value = { categoriesMap };
-  return <CategoryContext.Provider value={value}>{children}</CategoryContext.Provider>;
+	const value = { categoriesMap };
+	return <CategoryContext.Provider value={value}>{children}</CategoryContext.Provider>;
 };
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -9,11 +9,13 @@ const Shop = () => {
 	return (
 		<>
 			{Object.keys(categoriesMap).map((title) => {
+				// говорим, что в каждом элементе массива только для свойства title нужно вызывать функцию, которая  возвращет div с заголовком
 				return (
 					<Fragment key={title}>
 						<h2>{title}</h2>
 						<div className='products-container'>
 							{categoriesMap[title].map((product) => {
+								// и потом для свойства у которого заголовок будет совпадать с активным title вызвать фкнкцию, которая передает данные в следу пропс
 								return <ProductCard key={product.id} product={product} />;
 							})}
 						</div>
