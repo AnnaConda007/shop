83af8665bf1016fa08fa54429479175df5dc688b
Anna Hrulckova <annahrulkova@yandex.ru>
2023-07-06 21:58:47 +0400

    превью

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,28 +1,18 @@
 import { Fragment, useContext } from 'react';
 import { CategoryContext } from '../../contexts/category.context';
-import ProductCard from '../../comonents/product-card/product-card.component';
+import CategoryPrevviuw from '../../comonents/category-prevviuw/category-prevviuw.component';
 import './shop.styles.scss';
 
 const Shop = () => {
 	const { categoriesMap } = useContext(CategoryContext);
 
 	return (
-		<>
+		<div className='shop-container'>
 			{Object.keys(categoriesMap).map((title) => {
-				// говорим, что в каждом элементе массива только для свойства title нужно вызывать функцию, которая  возвращет div с заголовком
-				return (
-					<Fragment key={title}>
-						<h2>{title}</h2>
-						<div className='products-container'>
-							{categoriesMap[title].map((product) => {
-								// и потом для свойства у которого заголовок будет совпадать с активным title вызвать фкнкцию, которая передает данные в следу пропс
-								return <ProductCard key={product.id} product={product} />;
-							})}
-						</div>
-					</Fragment>
-				);
+				const products = categoriesMap[title];
+				return <CategoryPrevviuw key={title} title={title} products={products} />;
 			})}
-		</>
+		</div>
 	);
 };
 
