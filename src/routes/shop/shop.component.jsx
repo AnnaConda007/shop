eecc1c5e7a3e0b0d9296ff38a9438be8da2a4fe6
Anna Hrulckova <annahrulkova@yandex.ru>
2023-07-06 23:54:10 +0400

    маршр

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './shop.styles.scss';
 import CategoryESPrevviuw from '../categories-previuw/categories-prewuw.component';
+import Category from '../category/category.component';
 const Shop = () => {
 	return (
 		<Routes>
 			<Route index element={<CategoryESPrevviuw />} />
+			<Route path=':category' element={<Category />} />
+			{/* path=':category это спец динамический параметр, который отслеживает адрес строку и передает кажде значение дальще '*/}
 		</Routes>
 	);
 };
